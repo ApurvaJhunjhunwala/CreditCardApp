80302eaa7cf15f058166ee0bd849dacbb0e61ffa
ApurvaJhunjhunwala <43404362+ApurvaJhunjhunwala@users.noreply.github.com>
2021-06-20 19:12:36 +0100

    Updated Test Cases

--- src/test/java/com/creditcard/demotest/CreditCardTest.java
+++ src/test/java/com/creditcard/demotest/CreditCardTest.java
@@ -1,14 +1,9 @@
 package com.creditcard.demotest;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,15 +14,8 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import com.creditcard.model.CreditCard;
 import com.creditcard.repo.CreditCardRepository;
@@ -36,13 +24,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class CreditCardServiceTest extends CreditCardApplicationTest {
 
-	private MockMvc mockMvc;
 
-	@LocalServerPort
-	private int port;
-
-	@Autowired
-	private TestRestTemplate restTemplate;
 
 	@InjectMocks
 	CreditCardService cardService;
@@ -50,15 +32,12 @@ public class CreditCardServiceTest extends CreditCardApplicationTest {
 	@MockBean
 	private CreditCardRepository cardRepository;
 
-	@Autowired
-	private WebApplicationContext webApplicationContext;
 
 	static ObjectMapper om = new ObjectMapper();
 
 	@Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
-		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 	}
 
 	@Test
@@ -82,47 +61,19 @@ public class CreditCardServiceTest extends CreditCardApplicationTest {
 		verify(cardRepository, times(1)).save(addCard);
 
 	}
-
 	/*
-	 * @Test public void testControllerAddCard() { MockHttpServletRequest request =
-	 * new MockHttpServletRequest(); RequestContextHolder.setRequestAttributes(new
-	 * ServletRequestAttributes(request));
+	 * @Test public void testAllEmployees() { ResponseEntity<List<CreditCard>>
+	 * responseEntity;
 	 * 
-	 * when(cardRepository.save(any(CreditCard.class))).thenReturn(true);
+	 * responseEntity = this.restTemplate .getForObject("http://localhost:" + port +
+	 * "/api/v1/getAllCards", ResponseEntity<>); assertEquals(200,
+	 * responseEntity.getStatusCodeValue()); }
 	 * 
-	 * Employee employee = new Employee(1, "Lokesh", "Gupta",
-	 * "howtodoinjava@gmail.com"); ResponseEntity<Object> responseEntity =
-	 * employeeController.addEmployee(employee);
-	 * 
-	 * assertThat(responseEntity.getStatusCodeValue()).isEqualTo(201);
-	 * assertThat(responseEntity.getHeaders().getLocation().getPath()).isEqualTo(
-	 * "/1"); }
+	 * @Test public void testAddCard() { CreditCard card = new CreditCard(1,
+	 * "12345678903555", "TestApurva", 100); ResponseEntity<String> responseEntity =
+	 * this.restTemplate .postForEntity("http://localhost:" + port +
+	 * "/api/v1/addCard", card, String.class); assertEquals(201,
+	 * responseEntity.getStatusCodeValue()); }
 	 */
-	@Test
-	public void testFindAll() throws Exception {
-		// given
-		List<CreditCard> cards = Arrays.asList(new CreditCard(1, "12345678903555", "TestApurva", 100),
-				new CreditCard(2, "12345678403555", "TestApurva1", 200));
-
-		when(cardRepository.findAll()).thenReturn(cards);
-
-		mockMvc.perform(get("/api/v1/getAllCards")).andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-				.andExpect(status().isOk())
-				// .andExpect(jsonPath("$", hasSize(2)))
-				.andExpect(jsonPath("$[0].id", is("1"))).andExpect(jsonPath("$[0].cardNumber", is("12345678903555")))
-				.andExpect(jsonPath("$[0].nameOnCard", is("TestApurva"))).andExpect(jsonPath("$[0].balance", is(100)))
-				.andExpect(jsonPath("$[1].id", is("2"))).andExpect(jsonPath("$[1].cardNumber", is("12345678403555")))
-				.andExpect(jsonPath("$[1].nameOnCard", is("TestApurva1"))).andExpect(jsonPath("$[1].balance", is(200)));
-
-		verify(cardRepository, times(1)).findAll();
-	}
-
-	@Test
-	public void testAddCard() {
-		CreditCard card = new CreditCard(1, "12345678903555", "TestApurva", 100);
-		ResponseEntity<String> responseEntity = this.restTemplate
-				.postForEntity("http://localhost:" + port + "/api/v1/addCard", card, String.class);
-		assertEquals(201, responseEntity.getStatusCodeValue());
-	}
 
 }
